ce433fd4be8d7350247b91c02d359f4175e6c517
Robin Ang <l2obin@users.noreply.github.com>
2025-07-08 03:17:47 +0800

    feat: CRUD for table T

--- src/sqlite-test/sqlite-console.tsx
+++ src/sqlite-test/sqlite-console.tsx
@@ -11,7 +11,7 @@ function SqliteConsole() {
   // Add useState for array of object with a and b fields
   const [rows, setRows] = useState<{ a: number; b: number }[]>([]);
 
-  const fetchDatabase = async () => {
+  const initiateDatabase = async () => {
     try {
       console.log('Loading and initializing SQLite3 module...');
 
@@ -39,20 +39,13 @@ function SqliteConsole() {
         response.result.filename.replace(/^file:(.*?)\?vfs=opfs$/, '$1'),
       );
 
-      console.log('Query data with exec()');
-      const rows: { a: number; b: number }[] = [];
+      // Create a table if it does not exist
+      // a field is id and auto-incremented, b field is a random number
       await promiser('exec', {
         dbId,
-        sql: 'SELECT a, b FROM t ORDER BY a',
-        callback: (result) => {
-          if (result.row) {
-        // result.row is an array, map to object
-        rows.push({ a: result.row[0], b: result.row[1] });
-          }
-        },
+        sql: 'CREATE TABLE IF NOT EXISTS t(a INTEGER PRIMARY KEY AUTOINCREMENT, b INTEGER)',
       });
-      console.log('Fetched rows:', rows);
-      setRows(rows);
+      console.log('Creating a table...');
 
       return { promiser, dbId };
     } catch (err) {
@@ -64,49 +57,57 @@ function SqliteConsole() {
     }
   }
 
-  const callDatabase = async () => {
+  const fetchDatabase = async () => {
     try {
-      console.log('Loading and initializing SQLite3 module...');
-  
-      const promiser = await new Promise((resolve) => {
-        const _promiser = sqlite3Worker1Promiser({
-          onready: () => {
-            resolve(_promiser);
-          },
-        });
+      const { promiser, dbId } = await initiateDatabase();
+
+      console.log('Query data with exec()');
+
+      const rows: { a: number; b: number }[] = [];
+
+      await promiser('exec', {
+        dbId,
+        sql: 'SELECT a, b FROM t ORDER BY a',
+        callback: (result) => {
+          if (result.row) {
+        // result.row is an array, map to object
+        rows.push({ a: result.row[0], b: result.row[1] });
+          }
+        },
       });
-  
-      console.log('Done initializing. Running demo...');
-  
-      let response;
-  
-      response = await promiser('config-get', {});
-      console.log('Running SQLite3 version', response.result.version.libVersion);
-  
-      response = await promiser('open', {
-        filename: 'file:worker-promiser.sqlite3?vfs=opfs',
-      });
-      const { dbId } = response;
-      console.log(
-        'OPFS is available, created persisted database at',
-        response.result.filename.replace(/^file:(.*?)\?vfs=opfs$/, '$1'),
-      );
-  
-      await promiser('exec', { dbId, sql: 'CREATE TABLE IF NOT EXISTS t(a,b)' });
-      console.log('Creating a table...');
+      console.log('Fetched rows:', rows);
+
+      setRows(rows);
+
+      toast.success('Fetched rows successfully!');
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        err = new Error(err.result.message);
+      }
+      console.error(err.name, err.message);
+      
+      toast.error(`SQLite3 worker error: ${err.name} - ${err.message}`);
+    }
+  }
+
+  const insertSampleData = async () => {
+    try {
+      const { promiser, dbId } = await initiateDatabase();
   
       console.log('Insert some data using exec()...');
-      for (let i = 1; i <= 10; ++i) {
+      for (let i = 1; i <= 1; ++i) {
         await promiser('exec', {
           dbId,
-          sql: 'INSERT INTO t(a,b) VALUES (?,?)',
-          bind: [i, i * 2],
+          sql: 'INSERT INTO t(b) VALUES (?)',
+          bind: [Math.floor(Math.random() * 100000000)],
         });
       }
   
       await promiser('close', { dbId });
 
       toast.success('SQLite3 worker operations completed successfully!');
+
+      await fetchDatabase();
     } catch (err) {
       if (!(err instanceof Error)) {
         err = new Error(err.result.message);
@@ -117,11 +118,57 @@ function SqliteConsole() {
     }
   };
 
+  const clearDatabase = async () => {
+    try {
+      const { promiser, dbId } = await initiateDatabase();
+
+      console.log('Clearing the table...');
+      await promiser('exec', {
+        dbId,
+        sql: 'DELETE FROM t',
+      });
+
+      setRows([]);
+      toast.success('Database cleared successfully!');
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        err = new Error(err.result.message);
+      }
+      console.error(err.name, err.message);
+      toast.error(`SQLite3 worker error: ${err.name} - ${err.message}`);
+    }
+  }
+
+   const deleteAllTables = async () => {
+    try {
+      const { promiser, dbId } = await initiateDatabase();
+
+      console.log('Deleting all tables...');
+      
+      await promiser('exec', {
+        dbId,
+        sql: 'DROP TABLE IF EXISTS t',
+      });
+
+      console.log('All tables deleted successfully.');
+      toast.success('All tables deleted successfully!');
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        err = new Error(err.result.message);
+      }
+      console.error(err.name, err.message);
+      throw err;
+    }
+  }
+
+
   return (
     <>
-      <div className="flex flex-row items-center justify-center p-4">
-        <Button variant="outline" onClick={callDatabase}>Insert some data</Button>
-        <Button variant="outline" onClick={fetchDatabase}>Fetch all data</Button>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        <Button onClick={insertSampleData}>Insert some data</Button>
+        <Button variant="secondary" onClick={fetchDatabase}>Fetch all data</Button>
+        <Button variant="destructive" onClick={clearDatabase}>Clear Database</Button>
+        <Button variant="destructive" onClick={deleteAllTables}>Delete All Tables</Button>
       </div>
       <div className="p-4">
         <h3 className="font-semibold mb-2">Fetched Rows:</h3>
