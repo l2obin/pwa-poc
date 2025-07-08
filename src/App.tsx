16a7283c6212e442042431c344ff232bc2c657d8
Robin Ang <5276243+l2obin@users.noreply.github.com>
2025-07-08 14:49:26 +0800

    feat: added nav bar with toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Toaster } from 'sonner'
-import './App.css'
-import SqliteConsole from './sqlite-test/sqlite-console'
+import '@/App.css'
+import SqliteConsole from '@/sqlite-test/sqlite-console'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
 
 function App() {
 
   return (
-    <div>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <header className="flex items-baseline justify-between p-4 bg-gray-100 dark:bg-gray-800">
+        <h1 className="text-xl font-bold">SQLite WASM OPFS Perf Test</h1>
+        <ModeToggle />
+      </header>
       <SqliteConsole />
       <Toaster />
-    </div>
+    </ThemeProvider>
   )
 }
 
