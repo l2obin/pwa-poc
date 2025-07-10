8bd47defffe328ddda303ebfe98f502d0904fe6e
Robin Ang <l2obin@users.noreply.github.com>
2025-07-10 13:52:47 +0800

    refactor: remove commented codes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { MainNav } from '@/components/main-nav'
 import type { NavItem } from '@/types'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 const showDevtools = import.meta.env.VITE_TANSTACK_ROUTER_DEVTOOLS === 'true'
@@ -27,13 +27,3 @@ export const Route = createRootRoute({
     </>
   ),
 })
-
-
-      // <header className="flex items-baseline justify-between p-4 bg-gray-100 dark:bg-gray-900 fixed top-0 left-0 right-0 z-50">
-      //   <h1 className="text-xl font-bold">SQLite WASM OPFS Perf Test</h1>
-      //   <ModeToggle />
-      // </header>
-      // <main className="pt-16 px-4">
-      //   <SqliteConsole />
-      // </main>
-      // <Toaster />
